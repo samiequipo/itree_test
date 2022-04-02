f997546be091f71cf28d6281c5d18e872174401b
samiequipo <samiequipo@gmail.com>
2022-04-02 07:24:29 -0400

    First Repo

---
+++ 1.-payment_ibm_company.rb
@@ -0,0 +1,11 @@
+# La empresa IBM necesita saber cuanto pagar a cada uno de sus empleados, 
+# para ello necesita un algoritmo que calcule el salario a pagar, 
+# semanalmente, en base a las horas trabajadas y el salario por hora. 
+# Si el empleado trabajó mas de 48 horas en la semana,
+# el restante de horas debe calcularse con 50% mas, dado que son horas extras.
+
+
+
+def salary_payment
+
+end
